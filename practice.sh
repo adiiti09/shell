1fd8659568dd2f3151ba55dbfbb68cc55d977265
adiiti09 <89415764+adiiti09@users.noreply.github.com>
2024-04-23 06:20:33 +0000

    changed

--- practice.sh
+++ practice.sh
@@ -1,21 +1,35 @@
-echo "enter first number"
-read num1
-echo "enter second number"
-read num2
-echo "enter the operator"
-read op
-if [ $op == + ]
-then
-echo "$num1 + $num2 = $(($num1 + $num2))"
-elif [ $op == - ]
-then 
-echo "$num1 - $num2 = $(($num1 - $num2))"
-elif [ $op == * ]
-then
-echo "$num1 * $num2 = $(($num1 * $num2))"
-elif [ $op == / ]
-then
-echo "$num1 / $num2 = $(($num1 / $num2))"
-else
-echo "operator not found"
-fi
+#QUESTION1
+# echo "enter first number"
+# read num1
+# echo "enter second number"
+# read num2
+# echo "enter the operator"
+# read op
+# if [ $op == + ]
+# then
+# echo "$num1 + $num2 = $(($num1 + $num2))"
+# elif [ $op == - ]
+# then 
+# echo "$num1 - $num2 = $(($num1 - $num2))"
+# elif [ $op == * ]
+# then
+# echo "$num1 * $num2 = $(($num1 * $num2))"
+# elif [ $op == / ]
+# then
+# echo "$num1 / $num2 = $(($num1 / $num2))"
+# else
+# echo "operator not found"
+# fi
+
+#QUESTION2
+# echo "enter a number"
+# read a
+# if [ $a -gt 0 ]
+# then
+# echo "Number is positive"
+# elif [ $a -lt 0 ]
+# then
+# echo "Number is negative"
+# else
+# echo "Number is zero"
+# fi
